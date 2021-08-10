5c70db2daba6dfa8da1fc99eb7910974ae21d69f
Justin Joseph <jmjoseph20@hotmail.com>
2021-08-10 19:16:07 -0400

    Assignment 4 complete

--- TechJobsPersistent/Controllers/EmployerController.cs
+++ TechJobsPersistent/Controllers/EmployerController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TechJobsPersistent.Models;
 using TechJobsPersistent.ViewModels;
+using TechJobsPersistent.Data;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,25 +14,57 @@ namespace TechJobsPersistent.Controllers
 {
     public class EmployerController : Controller
     {
+        private JobDbContext _context;
+
+        public EmployerController(JobDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            List<Employer> employers = _context.Employers.ToList();
+
+            return View(employers);
         }
 
         public IActionResult Add()
         {
-            return View();
+            //List<Employer> employers = _context.Employers.ToList();
+
+            AddEmployerViewModel addEmployerViewModel = new AddEmployerViewModel();
+
+            return View(addEmployerViewModel);
         }
 
-        public IActionResult ProcessAddEmployerForm()
+        [HttpPost]
+        public IActionResult ProcessAddEmployerForm(AddEmployerViewModel addEmployerViewModel)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+
+                Employer employer = new Employer
+                {
+                    Name = addEmployerViewModel.Name,
+                    Location = addEmployerViewModel.Location,
+                };
+
+                _context.Employers.Add(employer); // This stages the data
+                _context.SaveChanges(); // This actually saves the data in the DB
+
+                return Redirect("/Employer");
+            }
+
+            return View(addEmployerViewModel);
         }
+
 
         public IActionResult About(int id)
         {
-            return View();
+            Employer employer = _context.Employers.Find(id);
+            return View(employer);
         }
+
     }
 }
--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,17 @@
 --Part 1
 
+Jobs.Id = int | Jobs.Name = longtext | Jobs.EmployerId = int
+
+
 --Part 2
 
+SELECT Name FROM Employers WHERE Location = "St. Louis City";
+
+
 --Part 3
+
+SELECT * FROM Skills
+      LEFT JOIN JobSkills ON Skills.Id = JobSkills.SkillId
+      WHERE JobSkills.JobId IS NOT NULL
+      ORDER BY name ASC;
 
